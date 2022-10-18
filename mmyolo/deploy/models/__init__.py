79258775bb94553a703476811283f09353d71597
triple-Mu <gpu@163.com>
2022-10-18 10:23:37 +0800

    Move package

--- mmyolo/deploy/models/__init__.py
+++ mmyolo/deploy/models/__init__.py
@@ -1,2 +1,5 @@
 # Copyright (c) OpenMMLab. All rights reserved.
 from . import dense_heads  # noqa: F401,F403
+from .object_detection import MMYOLO, YOLOObjectDetection
+
+__all__ = ['dense_heads', 'MMYOLO', 'YOLOObjectDetection']
